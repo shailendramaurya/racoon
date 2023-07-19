cda513e372ef434fb4659fe5c62d9489dab52bdd
Shailendra Maurya <87139652+shailendramaurya@users.noreply.github.com>
2023-07-19 09:06:39 +0530

    Update sw.js, cached index.html

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('racoon-cache').then(function(cache) {
       return cache.addAll([
+'index.html',
 'js/mdb.min.js',
 'css/mdb.min.css',
 'js/sweetalert2.js',
