bf62062df46a0bad7a5a1b318a75073976ddbe40
abrocks1120@gmail.com <abrocks1120@gmail.com>
2023-10-13 23:58:29 +0530

    reduce fragmentation

--- js/dynamicColors.js
+++ js/dynamicColors.js
@@ -1,13 +1,28 @@
-let colorIndex = 0;
+const themes = [
+	{ // Indigo
+		"--main": "#3949ab",
+		"--hover": "#303f9f",
+		"--focus": "rgba(57, 73, 171.0 .125)",
+		"--inverse": "#FFF",
+		"--hoverDark": "#3f51b5",
+		"--focusDark": "rgba(57, 73, 171, 0.25)"
+},
+	{ // Blue
+		"--main": "#1e88e5",
+		"--hover": "#1976d2",
+		"--focus": "rgba(30, 136, 229, 0.125)",
+		"--inverse": "#FFF",
+		"--hoverDark": "#2196f3",
+		"--focusDark": "rgba(30, 136, 229, 0.25)"
+}
+];
 
-fetch('assets/themes.json')
-	.then(_ => _.json())
-	.then(themes => {
-		setTimeout(() => {
-			if (colorIndex === themes.length) colorIndex = 0;
-			const theme = themes[colorIndex];
-			for (vars in theme)
-				document.querySelector(':root').style.setProperty(vars, theme[vars]);
-			colorIndex++;
-		}, 3000);
-	});
+let i = 0;
+
+setInterval(() => {
+	if (i === themes.length) i = 0;
+	const theme = themes[i];
+	for (vars in theme)
+		document.querySelector(':root').style.setProperty(vars, theme[vars]);
+	i++;
+}, 2000);
